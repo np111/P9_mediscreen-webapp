bea99605cbe6760d1ddfd1a9f37f9a1e87107fd6
np111 <oc.np111@protonmail.com>
2021-07-29 17:25:20 +0200

    Ensures that the CSS for notifications is present on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import {AppProps} from 'next/app';
 import React from 'react';
 import '../assets/styles/app.global.scss';
 
+require('antd/lib/notification/style/index.less'); // FIX: ensures that the CSS for notifications is present on all pages
+
 export default function MyApp({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />;
 }
